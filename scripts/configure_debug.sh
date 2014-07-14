eae8290ba05ac6a4d3017aa007e3d12fc0c6cb73
James Hilliard <james.hilliard1@gmail.com>
2014-07-14 08:36:38 -0400

    enable more drivers

--- scripts/configure_debug.sh
+++ scripts/configure_debug.sh
@@ -1,4 +1,4 @@
 cd ..
 DIR=`pwd`
 cd source
-./configure --prefix="$DIR/output/debug" CFLAGS="-O0 -ggdb" LIBCURL_CFLAGS="-I/usr/local/opt/curl/include/curl" LIBCURL_LIBS="-L/usr/local/opt/curl/lib -lcurl" LIBUSB_CFLAGS="-I/usr/local/opt/libusb/include/libusb-1.0" LIBUSB_LIBS="-L/usr/local/opt/libusb/lib -lusb-1.0" --enable-icarus
+./configure --prefix="$DIR/output/debug" CFLAGS="-O0 -ggdb" LIBCURL_CFLAGS="-I/usr/local/opt/curl/include/curl" LIBCURL_LIBS="-L/usr/local/opt/curl/lib -lcurl" LIBUSB_CFLAGS="-I/usr/local/opt/libusb/include/libusb-1.0" LIBUSB_LIBS="-L/usr/local/opt/libusb/lib -lusb-1.0" --enable-icarus --enable-bflsc --enable-bitforce --enable-modminer --enable-avalon --enable-opencl --enable-bitfury --enable-hashfast --enable-klondike --enable-bab --enable-drillbit --enable-avalon2 --enable-minion
